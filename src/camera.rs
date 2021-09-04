a6157180262920c6f1917eee864bcab9de362824
Yuki Kodama <endflow.net@gmail.com>
2021-09-04 22:56:14 +0900

    refactoring

---
+++ src/camera.rs
@@ -0,0 +1,30 @@
+use image::RgbImage;
+use nokhwa::Camera;
+use std::thread;
+
+pub fn capture(tx: flume::Sender<RgbImage>) {
+    thread::spawn(move || {
+        let fps = 10;
+        let mut camera = Camera::new_with(
+            4,
+            640,
+            480,
+            fps,
+            nokhwa::FrameFormat::MJPEG,
+            nokhwa::CaptureAPIBackend::Auto,
+        )
+        .unwrap();
+        camera.open_stream().unwrap();
+        loop {
+            let frame = camera.frame().unwrap();
+            println!(
+                "Captured frame {}x{} @ {}FPS size {}",
+                frame.width(),
+                frame.height(),
+                fps,
+                frame.len()
+            );
+            tx.send(frame).unwrap()
+        }
+    });
+}
---
+++ src/vertex.rs
@@ -0,0 +1,5 @@
+#[derive(Copy, Clone)]
+pub(crate) struct Vertex {
+    pub(crate) position: [f32; 2],
+    pub(crate) tex_coords: [f32; 2],
+}
